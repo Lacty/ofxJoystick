82ac587d352e75198e21422e7dd889518258bbdc
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-08-10 15:45:10 +1000

    Fixes for Linux
    We need to know the state of a button when the Joystick Starts up.

--- src/ofxJoystick.cpp
+++ src/ofxJoystick.cpp
@@ -1,47 +1,167 @@
 ﻿
 #include "ofxJoystick.h"
+#ifdef __linux__
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/joystick.h>
+int read_event(int fd, struct js_event *event)
+{
+    ssize_t bytes;
 
+    bytes = read(fd, event, sizeof(*event));
+
+    if (bytes == sizeof(*event))
+        return 0;
+
+    /* Error, could not read full event. */
+    return -1;
+}
+
+/**
+ * Returns the number of axes on the controller or 0 if an error occurs.
+ */
+size_t get_axis_count(int fd)
+{
+    __u8 axes;
+
+    if (ioctl(fd, JSIOCGAXES, &axes) == -1)
+        return 0;
+
+    return axes;
+}
+
+/**
+ * Returns the number of buttons on the controller or 0 if an error occurs.
+ */
+size_t get_button_count(int fd)
+{
+    __u8 buttons;
+    if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
+        return 0;
+
+    return buttons;
+}
+
+size_t get_axis_state(struct js_event *event, struct axis_state axes[3])
+{
+    size_t axis = event->number / 2;
+
+    if (axis < 3)
+    {
+        if (event->number % 2 == 0)
+            axes[axis].x = event->value/32767.f;
+        else
+            axes[axis].y = event->value/32767.f;
+    }
+
+    return axis;
+}
+
+#endif
 
 ofxJoystick::~ofxJoystick() {
   ofRemoveListener(ofEvents().update, this, &ofxJoystick::update);
 }
 
 void ofxJoystick::updateState() {
+#ifdef __linux__
+  if (isConnect_ == false)
+  {
+    char device[255];
+
+    sprintf(device,"/dev/input/js%d", id_);
+     js_ = open(device, O_NONBLOCK);
+    name_= std::string(device);
+    printf("\n");
+    if (js_ == -1)
+        return;
+
+    isConnect_ = true;
+    //perror("Could not open joystick");
+
+    struct JS_DATA_TYPE jso;
+    if(read(js_, &jso, JS_RETURN))
+    {
+        printf("%x", jso.buttons );
+         for (int i=0;i<15;i++)
+            if(jso.buttons & 1 << i)
+             {
+                button[i] = true;
+                printf("%d Held\n",i);
+            }
+    }
+  }
+  #else
   isConnect_ = (glfwJoystickPresent(id_) == GL_TRUE);
   if (isConnect_) {
     name_ = glfwGetJoystickName(id_);
   }
+
+
+
+  #endif
 }
 
 void ofxJoystick::updateAxis() {
+  #ifdef __linux__
+  axis_.clear();
+  //axis_ = vector<float>(&axes[0], &axes[axisNum_]);
+  for (int axis=0; axis<axisNum_;axis++)
+  {
+    //std::cout <<"  "<< axes[axis].x << " " << axes[axis].y;
+    axis_.push_back(axes[axis].x);
+    axis_.push_back(axes[axis].y);
+
+  }
+  //std::cout << std::endl;
+  #else
   const float* axis = glfwGetJoystickAxes(id_, &axisNum_);
   axis_ = vector<float>(&axis[0], &axis[axisNum_]);
+#endif // __linux__
 }
-  
+
 void ofxJoystick::updateButton() {
   pressed_.clear();
   release_.clear();
-  
+#ifdef __linux__
+#else
   const unsigned char* button = glfwGetJoystickButtons(id_, &buttonNum_);
-  
+#endif
   for (int i = 0; i < buttonNum_; i++) {
+    //std::cout << i << " " <<(int16_t)button[i] << "  ";
     switch(button[i]) {
+
       case GLFW_PRESS : {
         if (pushing_.find(i) == pushing_.end()) {
           pressed_.emplace(i);
+
         }
+        //std::cout << i << " Pressed!" << std::endl;
         pushing_.emplace(i);
         break;
       }
       case GLFW_RELEASE : {
         if (pushing_.find(i) != pushing_.end()) {
           release_.emplace(i);
+        //std::cout  << i << " Released!" << std::endl;
           pushing_.erase(pushing_.find(i));
         }
         break;
       }
+      	case GLFW_REPEAT:
+		{
+			if (pushing_.find(i) == pushing_.end()) {
+				pressed_.emplace(i);
+			}
+			 // std::cout << i << " Pressed!" << std::endl;
+			pushing_.emplace(i);
+
+			break;
+		}
     }
   }
+  //std::cout << std::endl;
 }
 
 void ofxJoystick::setup(int JoyId) {
@@ -52,11 +172,20 @@ void ofxJoystick::setup(int JoyId) {
     ofLog() << "Joypad [" << id_ << "] : disconnected";
     return;
   }
-  
+
+  #ifdef __linux__
+
+  #else
   name_ = glfwGetJoystickName(id_);
-  glfwGetJoystickButtons(id_, &buttonNum_);
-  glfwGetJoystickAxes(id_, &axisNum_);
-  
+  #endif
+  //avoid crash when accessing data on first call to ofApp::update()
+  updateAxis();
+  updateButton();
+  axisNum_ = get_axis_count(js_);
+  buttonNum_ = get_button_count(js_);
+  //glfwGetJoystickButtons(id_, &buttonNum_);
+  //glfwGetJoystickAxes(id_, &axisNum_);
+
   ofAddListener(ofEvents().update, this, &ofxJoystick::update);
 
   ofLog() << "JoyPad connected : " << name_;
@@ -67,13 +196,35 @@ void ofxJoystick::setup(int JoyId) {
   updateAxis();
   updateButton();
 }
-  
+
 void ofxJoystick::update(ofEventArgs &args) {
+ #ifdef __linux__
+  struct js_event event;
+  size_t axis;
+  while (read_event(js_, &event) == 0)
+    {
+
+        switch (event.type)
+        {
+            case JS_EVENT_BUTTON:
+                printf("Button %u %s\n", event.number, event.value ? "pressed" : "released");
+                button[event.number] = event.value ? GLFW_PRESS : GLFW_RELEASE;
+                break;
+            case JS_EVENT_AXIS:
+                axis = get_axis_state(&event, axes);
+
+                break;
+            default:
+                /* Ignore init events. */
+                break;
+        }
+    }
+  #endif
   updateState();
   updateAxis();
   updateButton();
 }
-  
+
 bool ofxJoystick::isConnect() const {
   return isConnect_;
 }
@@ -85,35 +236,35 @@ int ofxJoystick::getId() const {
 const string& ofxJoystick::getName() const {
   return name_;
 }
-  
+
 int ofxJoystick::getButtonNum() const {
   return buttonNum_;
 }
-  
+
 int ofxJoystick::getAxisNum() const {
   return axisNum_;
 }
-  
+
 float ofxJoystick::getAxis(int num) const {
   if (!isConnect_) return 0;
   if (num > axisNum_) return 0;
   return axis_[num];
 }
-  
+
 bool ofxJoystick::isPressed(int button) const {
   if (pressed_.find(button) == pressed_.end()) {
     return false;
   }
   return true;
 }
-  
+
 bool ofxJoystick::isPushing(int button) const {
   if (pushing_.find(button) == pushing_.end()) {
     return false;
   }
   return true;
 }
-  
+
 bool ofxJoystick::isRelease(int button) const {
   if (release_.find(button) == release_.end()) {
     return false;
@@ -127,3 +278,4 @@ bool ofxJoystick::anyButton() const {
   }
   return false;
 }
+
--- src/ofxJoystick.h
+++ src/ofxJoystick.h
@@ -3,34 +3,43 @@
 #include "ofMain.h"
 
 #include "GLFW/glfw3.h"
-
+struct axis_state {
+    short x, y;
+};
 
 class ofxJoystick {
 private:
   bool isConnect_;
+#ifdef __linux__
+    int js_;
 
+
+    struct axis_state axes[3] = {0};
+    unsigned char button[256] = {0};
+
+#endif // __linux__
   int    id_;
   string name_;
 
   int buttonNum_;
   int axisNum_;
-  
+
   set<int> pressed_;
   set<int> pushing_;
   set<int> release_;
-  
+
   vector<float> axis_;
-  
+
   void updateState();
   void updateAxis();
   void updateButton();
-  
+
   void update(ofEventArgs &args);
 
 public:
   ofxJoystick() = default;
   ~ofxJoystick();
-  
+
   // JoyId : usually give GLFW_SOYSTICK_1
   // when you connect only one Gamepad
   void setup(int JoyId);
@@ -40,21 +49,21 @@ public:
   bool isConnect() const;
 
   int getId() const;
-  
+
   // return your Gamepad name
   const string& getName() const;
 
   // return number of Button and Axis
   int getButtonNum() const;
   int getAxisNum() const;
-  
+
   // return axis, if disconnected return 0
   float getAxis(int num) const;
-  
+
   // button func
   bool isPressed(int button) const;
   bool isPushing(int button) const;
   bool isRelease(int button) const;
-  
+
   bool anyButton() const;
 };
