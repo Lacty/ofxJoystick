48991222579cad1c65b782b4b4cae5d6d68e3af9
CarloCattano <freevibes@gmail.com>
2024-06-29 09:32:49 +0200

    Fix compilation issue with set and namespaces

--- src/ofxJoystick.h
+++ src/ofxJoystick.h
@@ -4,57 +4,58 @@
 
 #include "GLFW/glfw3.h"
 
+using namespace std;
 
 class ofxJoystick {
 private:
-  bool isConnect_;
+	bool isConnect_;
 
-  int    id_;
-  string name_;
+	int id_;
+	string name_;
 
-  int buttonNum_;
-  int axisNum_;
-  
-  set<int> pressed_;
-  set<int> pushing_;
-  set<int> release_;
-  
-  vector<float> axis_;
-  
-  void updateState();
-  void updateAxis();
-  void updateButton();
-  
-  void update(ofEventArgs &args);
+	int buttonNum_;
+	int axisNum_;
+
+	set<int> pressed_;
+	set<int> pushing_;
+	set<int> release_;
+
+	vector<float> axis_;
+
+	void updateState();
+	void updateAxis();
+	void updateButton();
+
+	void update(ofEventArgs & args);
 
 public:
-  ofxJoystick() = default;
-  ~ofxJoystick();
-  
-  // JoyId : usually give GLFW_SOYSTICK_1
-  // when you connect only one Gamepad
-  void setup(int JoyId);
+	ofxJoystick() = default;
+	~ofxJoystick();
 
-  // if connected return true
-  // disconnected return false
-  bool isConnect() const;
+	// JoyId : usually give GLFW_SOYSTICK_1
+	// when you connect only one Gamepad
+	void setup(int JoyId);
 
-  int getId() const;
-  
-  // return your Gamepad name
-  const string& getName() const;
+	// if connected return true
+	// disconnected return false
+	bool isConnect() const;
 
-  // return number of Button and Axis
-  int getButtonNum() const;
-  int getAxisNum() const;
-  
-  // return axis, if disconnected return 0
-  float getAxis(int num) const;
-  
-  // button func
-  bool isPressed(int button) const;
-  bool isPushing(int button) const;
-  bool isRelease(int button) const;
-  
-  bool anyButton() const;
+	int getId() const;
+
+	// return your Gamepad name
+	const string & getName() const;
+
+	// return number of Button and Axis
+	int getButtonNum() const;
+	int getAxisNum() const;
+
+	// return axis, if disconnected return 0
+	float getAxis(int num) const;
+
+	// button func
+	bool isPressed(int button) const;
+	bool isPushing(int button) const;
+	bool isRelease(int button) const;
+
+	bool anyButton() const;
 };
